7b273a251878c9169ba4bfd1dbe8391d6b2dc82f
Albin Rodriguez <ealbinu@gmail.com>
2024-08-19 17:40:51 -0600

    U3A

--- src/components/blocks/TextLine.vue
+++ src/components/blocks/TextLine.vue
@@ -1,9 +1,9 @@
 <template>
 
-<div class="text-line bg-slate-200 [&>*:nth-child(odd)]:bg-slate-300/60 ">
+<div class="text-line bg-slate-100 [&>*:nth-child(odd)]:bg-slate-300/60 p-1 border-2 border-slate-300">
     <div v-for="(item, index) in texts" class="flex gap-2 items-center leading-5 py-1 px-2">
         <div class="text-slate-400  p-1 w-10">{{ index+1 }}</div>
-        <div class="text-xl">{{ item }}</div>
+        <div class="text-xl" v-html="item"></div>
     </div>
 </div>
 </template>
